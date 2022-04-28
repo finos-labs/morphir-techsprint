454dc22052c141b76051510b20de1b72902970ab
David Al-Kanani <david.al-kanani@regnosys.com>
2022-04-28 19:15:25 +0100

    add back end express server
    Signed-off-by: David Al-Kanani <david.al-kanani@regnosys.com>

---
+++ report-execution/backend/server/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send([
+    { tradeDate: "20220210", eventTimestamp: 123456 },
+    { tradeDate: "20220215", eventTimestamp: 123457 },
+    { tradeDate: "20220220", eventTimestamp: 123458 },
+    { tradeDate: "20210210", eventTimestamp: 123459 },
+  ]);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
