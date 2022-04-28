e61c11b4f76bcc7b350333e87afe5eddfb5d9852
David Al-Kanani <david.al-kanani@regnosys.com>
2022-04-28 13:44:42 +0100

    add upload button
    Signed-off-by: David Al-Kanani <david.al-kanani@regnosys.com>

--- report-execution-ui/src/app/app.component.ts
+++ report-execution-ui/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Report Execution';
+
+  upload(event: Event) {
+    console.log(event);
+  }
 }
--- report-execution-ui/src/app/app.module.ts
+++ report-execution-ui/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NoopAnimationsModule, MatToolbarModule],
+  imports: [
+    BrowserModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
