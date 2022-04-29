4ff5f82ecba4d28f723f5c9859c239c7f9f7fb1b
Nwokafor Choongsaeng <nwokafor.choongsaeng@morganstanley.com>
2022-04-29 12:14:25 +0000

    scala server changes
    Signed-off-by: Nwokafor Choongsaeng <nwokafor.choongsaeng@morganstanley.com>

--- report-execution/backend/morphir-scala/src/main/scala/morphir/App.scala
+++ report-execution/backend/morphir-scala/src/main/scala/morphir/App.scala
@@ -1,12 +1,26 @@
 package morphir
 
-/**
- * @author ${user.name}
- */
-object App {
-  
-  def main(args : Array[String]) {
-    println( "Hello World!" )
-  }
+import morphir.sdk.LocalDate
+import morphir.techsprint.minimal.BasicTypes.{Date, ZonedDateTime}
+import morphir.techsprint.minimal.Enum.EventTimestampQualificationEnum.EventCreationDateTime
+import morphir.techsprint.minimal.Rule
+import morphir.techsprint.minimal.Type.{EventTimestamp, ReportableEvent, Trade, TradeState, WorkflowStep}
 
+object App {
+  def main(args: Array[String]): Unit = {
+    val localDate = java.time.LocalDate.now()
+    val localTime = java.time.LocalTime.now()
+    val zone = "UTC"
+    val report = Rule.cFTCPart45TransactionReport(
+      ReportableEvent(WorkflowStep(
+        List(EventTimestamp(
+          ZonedDateTime(localDate, localTime, zone),
+          EventCreationDateTime)
+        )),
+        TradeState(Trade(Date(23, 1, 2020)))
+      )
+    )
+
+    println(report)
+  }
 }
